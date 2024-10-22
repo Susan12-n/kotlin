9a3e2c60c00ca2f11f08986e9db37f258d0db931
Susan12-n <ngesasuzan@gmail.com>
2024-10-22 12:27:14 +0300

    kottlin

---
+++ lesson1.kt
@@ -0,0 +1,9 @@
+fun main(){
+    println("Hello Kotlin")
+    // kotlin datatypes 
+val mynumber=5 //Int
+val weight=50.5 //Double
+val letter='D' // Char
+val myname="Jayne" //String
+val isComing = False //Boolean
+}
